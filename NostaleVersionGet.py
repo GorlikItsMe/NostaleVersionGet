3b241c0b40a7699df8e13116e54890a13eaf8865
Gorlik <33355238+GorlikItsMe@users.noreply.github.com>
2020-10-23 11:09:27 +0200

    HotFix

--- NostaleVersionGet.py
+++ NostaleVersionGet.py
@@ -18,13 +18,13 @@ def NostaleVersionGet(filecache=False):
             if (f['file'] == "NostaleClientX.exe"):
                 download_file("http://patches.gameforge.com"+f['path'], f['file'])
                 hashNostaleClientX = md5(f['file'])
-                os.remove(f['file']) 
             if (f['file'] == "NostaleClient.exe"):
                 download_file("http://patches.gameforge.com"+f['path'], f['file'])
                 hashNostaleClient = md5(f['file'])
-                os.remove(f['file']) 
 
         version = fileversion("NostaleClient.exe")
+        os.remove("NostaleClient.exe") 
+        os.remove("NostaleClientX.exe") 
         data = {"savetime": int(time.time()), "hashNostaleClientX": hashNostaleClientX, "hashNostaleClient": hashNostaleClient, "version": version}
         if(filecache):
             fp = open('NostaleVersionCache.json',  "w+")
@@ -39,6 +39,7 @@ def download_file(url, filename):
         with open(filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192): 
                 f.write(chunk)
+            f.close()
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -51,4 +52,5 @@ def fileversion(pename):
     pe = PE(pename)
     verinfo = pe.VS_FIXEDFILEINFO[0]
     filever = (verinfo.FileVersionMS >> 16, verinfo.FileVersionMS & 0xFFFF, verinfo.FileVersionLS >> 16, verinfo.FileVersionLS & 0xFFFF)
+    pe.close()
     return  "%d.%d.%d.%d" % filever
